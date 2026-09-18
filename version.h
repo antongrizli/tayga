12d71852bc88d4ad6626d78ea63974854af2fa19
Anton Konikov <antongrizli@gmail.com>
2026-09-18 15:30:17 +0200

    fix: add missing network headers in pref64 unit tests and quote docker workflow step names

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef __TAYGA_VERSION_H__
 #define __TAYGA_VERSION_H__
 
-#define TAYGA_VERSION "0.9.6-31-gf15a93d-dirty"
+#define TAYGA_VERSION "0.9.6-32-g37f3fbf-dirty"
 #define TAYGA_BRANCH  "main"
-#define TAYGA_COMMIT  "f15a93d01e13c029ceb7e9b12ae74b35e7c1ce13"
+#define TAYGA_COMMIT  "37f3fbff06d508726c6e59516df68f530d387670"
 
 #endif /* #ifndef __TAYGA_VERSION_H__ */
